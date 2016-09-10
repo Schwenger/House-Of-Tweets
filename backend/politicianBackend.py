ce8634c5416418a4eaf0651e046ed984dbe68942
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-09-10 06:02:40 +0200

    Backend: dead code for breakfast

--- backend/politicianBackend.py
+++ backend/politicianBackend.py
@@ -21,36 +21,6 @@ class PoliticianBackend:
 		
 	def getAllPoliticians(self):
 		return self.polList
-		
-	def createPolitician(self, pid, name, party, twittering, self_bird, citizen_bird, cv, images):
-		dic = {}
-		#dic["id"] = pid
-		dic["name"] = name 
-		dic["party"] = party
-		dic["twittering"] = twittering
-		dic["self_bird"] = self_bird
-		dic["citizen_bird"] = citizen_bird
-		dic["cv"] = cv
-		
-		im = None
-		if images is not None:
-			if "http:" not in images and "https:" not in images:
-				images = "http://" + images
-				
-			print("images = " + images)
-			fullfilename = os.path.join("../ext/images/politicians/", pid + ".jpg")
-			urllib.request.urlretrieve(images, fullfilename)
-			self.cutImage(fullfilename, fullfilename, (500,500))
-			thumb = os.path.join("../ext/images/politicians/", "t" +pid + ".jpg")
-			self.cutImage(fullfilename, thumb, (75,75))
-			
-			im = {}
-			im["pathToThumb"] = pid + ".jpg"
-			im["pathToImage"] = "t" + pid + ".jpg"
-			
-		dic["images"] = im
-		
-		return (pid, dic)
 
 	def getAllTwitteringPoliticians(self):
 		res = []
