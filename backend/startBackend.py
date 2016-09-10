dfb443cfdb260f23fe9783e79bfc740dcecf1d21
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-09-10 17:23:08 +0200

    Backend: support starting up

--- backend/startBackend.py
+++ backend/startBackend.py
@@ -7,18 +7,16 @@ from politicianBackend import PoliticianBackend
 from citizenQueueAdapter import CitizenQueueAdapter
 from birdBackend import BirdBackend
 from citizenBirdQueueAdapter import CitizenBirdQueueAdapter
+from twitter import RealTwitterInterface
 
 birdBack = BirdBackend()
 polBack = PoliticianBackend()
-#follow = ["4718199753"]#["29033470"]
-follow = []
-follow.extend(polBack.getAllTwitteringPoliticians())
-#print(follow)
-
+follow = ["4718199753", "774336282101178368"]
+# follow.extend(polBack.getAllTwitteringPoliticians())
+print("Configured to follow {} accounts.".format(len(follow)))
 
 queue = mq.Batcher(mq.RealQueue("tweets"))
-
-twi = TwitterConnection(queue, follow, polBack, birdBack)
+twi = TwitterConnection(queue, follow, polBack, birdBack, RealTwitterInterface())
 
 c = CitizenQueueAdapter(twi)
 c.start()
